d9e4ef375721dfc58229fbd31ed709f85e31f072
ax0n <128418336+ax0n5-78@users.noreply.github.com>
2023-09-23 10:26:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,26 @@ serase.innerHTML = '<ul><li>'+menes.join('</li><li>')+'</li></ul>'
 
 function eita(){
     persona = document.getElementById('menePersona').value
+    persona = persona.charAt(0).toUpperCase() + persona.slice(1).toLowerCase()
 
-    menes.push(persona)
+    if(menes.indexOf(persona) == -1){
+        menes.push(persona)
+    } else{
+        alert('Hermano, já tem esse mene na list')
+    }
+
+    serase.innerHTML = '<ul><li>'+menes.join('</li><li>')+'</li></ul>'
+}
+
+function que(){
+    persona = document.getElementById('menePersona').value
+    persona = persona.charAt(0).toUpperCase() + persona.slice(1).toLowerCase()
+
+    if(menes.indexOf(persona) == -1){
+        alert('Tá lokeando, tem nada pra remover, doido kkkk')
+    } else{
+        menes.splice(menes.indexOf(persona),1)
+    }
 
     serase.innerHTML = '<ul><li>'+menes.join('</li><li>')+'</li></ul>'
 }
